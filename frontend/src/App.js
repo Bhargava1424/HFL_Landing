0008b4526bfed5d2518181bcb5314e4fac37e026
Bhargava1424 <sribhargava2002@gmail.com>
2024-05-02 14:16:25 +0530

    first commit
    installed daisyUI
    setup Frontend and Server

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Guys remove this shit and make a base by changing global settings
+        </p>
+        <p>
+        <button className="btn btn-secondary">DaisyUI installed and is Workin!!</button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.txt
@@ -0,0 +1,27 @@
+## HFL Landing Page ##
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+- Node.js and npm (Node Package Manager)
+- MongoDB (Locally or via MongoDB Atlas)
+
+## Installation
+
+Follow these steps to get your development environment set up:
+
+1. **Clone the repository**
+
+   git clone [repository-url]
+   cd HFL
+
+2.  cd frontend
+    npm install
+
+3.  npm start
+
+4.  cd ..
+
+5.  cd server
+    npm install
+    node server.js
