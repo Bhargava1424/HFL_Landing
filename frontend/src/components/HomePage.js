57c846dc24c613c0a8a964872508d285bf0c18d0
kundannanubala <nanubalakundan.007@gmail.com>
2024-05-03 14:20:13 +0530

    partial scroll finished

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,12 +1,17 @@
 // src/components/Homepage.js
 
 import React from 'react';
+import CurrencyScroll from './CurrencyScroll';
 
 const Homepage = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-2">Empty Homepage</h1>
-      <button className="btn btn-warning">Click Me</button> {/* Change btn-primary to btn-warning */}
+    <div>
+      <header>
+        {/* Navigation bar here */}
+      </header>
+        <CurrencyScroll />
+        {/* Rest of the home page content */}
+      
     </div>
   );
 };
