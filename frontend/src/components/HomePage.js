788c591a742ada7aac70190d2cb0bc9460c555b7
sreesha baraar <10152002ssb@gmail.com>
2024-05-09 14:00:27 +0530

    banckscroll component
    added bankscroll component

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,6 +6,7 @@ import NavBar from './NavBar'
 import ImageSlider from './ImageSlider'
 import StatsDisplay from './StatsDisplay';
 import Testimony from './Testimony';
+import BanksScroll from './BanksScroll';
 
 
 const Homepage = () => {
@@ -19,6 +20,7 @@ const Homepage = () => {
       <ImageSlider/>
       <StatsDisplay/>
       <Testimony/>
+      <BanksScroll/>
         
         {/* Rest of the home page content */}
       
