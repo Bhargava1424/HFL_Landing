f75c905db87b2fafe863cc562273d05bdf5f743c
Bhargava1424 <sribhargava2002@gmail.com>
2024-06-09 12:39:05 +0530

    footer fixed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import HFLLogo from '../assets/HFLlogo.jpg';
 import { useNavigate } from 'react-router-dom';
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa'; // Importing icons from react-icons
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter, FaMapMarkerAlt } from 'react-icons/fa'; // Importing icons from react-icons
 
 const Footer = () => {
     const navigate = useNavigate();
     return (
         <footer className="bg-[#e3a833] text-black py-2">
             <div className="container px-4 mx-auto text-xs md:text-sm">
-                <div className="flex flex-col items-start justify-between lg:flex-row">
-                    <div className="flex flex-col">
-                        <div>
+                <div className="flex flex-col lg:flex-row items-center justify-between lg:items-start">
+                    <div className="flex flex-col items-center lg:items-center">
+                        <div className="flex flex-col items-center">
                             <div className='flex justify-center'>
                                 <img
                                     src={HFLLogo}
@@ -20,13 +20,17 @@ const Footer = () => {
                             </div>
                             <div className="flex justify-center font-bold md:mt-2">Hyderabad Forex Limited</div>
                         </div>
-                        <div>
-                            6-3-1090/A/24, LG - 2, Olbee Centre, Raj Bhavan Rd, Somajiguda, Hyderabad, Telangana 500082
+                        <div className="flex justify-center mt-4 lg:mt-2">
+                            <div className="text-center">
+                                <div>
+                                    6-3-1090/A/24, LG - 2, Olbee Centre, Raj Bhavan Rd, Somajiguda, Hyderabad, Telangana 500082
+                                </div>
+                                <div>
+                                    # 41st Floor, Telecom Housing Society, Gachibowli, Telangana 500032
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            # 41st Floor, Telecom Housing Society, Gachibowli, Telangana 500032
-                        </div>
-                        <div className="bottom-0 mt-12 text-xs text-center border-t border-black md:pt-2 md:text-sm">
+                        <div className="mt-4 text-xs text-center border-t border-black lg:mt-12 lg:text-sm lg:pt-2">
                             <div>CIN L8989787876767512</div>
                             <div>
                                 © 2024 Hyderabad Forex Private Limited. All Rights Reserved |{' '}
@@ -48,8 +52,8 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex-row items-start hidden mt-5 space-x-1 space-y-1 text-xs font-bold md:flex md:flex-col md:text-sm md:mt-12">
-                        <button onClick={() => navigate('/')} className="flex items-center ml-1 hover:underline">
+                    <div className="flex flex-row items-center mt-6 md:pt-8 space-x-6 lg:flex-col lg:items-start lg:space-y-4 lg:space-x-0 lg:mt-0">
+                        <button onClick={() => navigate('/')} className="flex items-center hover:underline">
                             Home <span className="ml-1">↗</span>
                         </button>
                         <button onClick={() => navigate('/Getanymoney')} className="flex items-center hover:underline">
@@ -65,8 +69,8 @@ const Footer = () => {
                             FAQs <span className="ml-1">↗</span>
                         </button>
                     </div>
-                    <div className="flex flex-row items-center mt-6 space-x-20 space-y-2 md:flex-col md:mt-12 md:mr-10">
-                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className=" ml-20 text-black">
+                    <div className="flex flex-row items-center mt-6 md:pt-8 space-x-6 lg:flex-col lg:items-start lg:space-y-4 lg:space-x-0 lg:mt-0">
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-black">
                             <FaFacebookF size={24} />
                         </a>
                         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-black">
@@ -79,28 +83,31 @@ const Footer = () => {
                             <FaTwitter size={24} />
                         </a>
                     </div>
-                    <div className="flex-col items-center hidden space-y-2 lg:flex">
+                    <div className="flex flex-row items-center mt-6 md:pt-6 space-x-6 lg:flex-col lg:items-start lg:space-y-4 lg:space-x-0 lg:mt-0">
                         <div className="text-center">
                             <span className='font-bold md:text-lg'>Store Locations</span>
                         </div>
-                        <div className="flex justify-center gap-24 space-x-4">
-                            <span className='font-medium'>Somajiguda</span>
-                            <span className='font-medium'>Gachibowli</span>
+                        <div className="flex flex-row space-x-6 lg:flex-col lg:space-x-0 lg:space-y-4">
+                            <a
+                                href="https://www.google.com/maps/place/Hyderabad+Forex+Limited/@17.4237138,78.4563976,17z/data=!3m1!4b1!4m6!3m5!1s0x3bcb974c629a05bf:0x1f2ad73d4257ca81!8m2!3d17.4237087!4d78.4589779!16s%2Fg%2F1tfph83v?entry=ttu"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center bg-[#e3c281] text-black px-4 py-2 rounded-md shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#eec064]"
+                            >
+                                <FaMapMarkerAlt className="text-2xl md:text-5xl" />
+                                <span className="font-medium ml-2">Somajiguda</span>
+                            </a>
+                            <a
+                                href="https://www.google.com/maps/place/HYDERABAD+FOREX+LIMITED/@17.4358344,78.3642202,17z/data=!3m2!4b1!5s0x3bcb93ec2b90a663:0xfadde25fa15fec9e!4m6!3m5!1s0x3bcb93ec2b8933c7:0xfe8d87caf8af1578!8m2!3d17.4358293!4d78.3668005!16s%2Fg%2F11gghcp0qb?entry=ttu"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center bg-[#e3c281] text-black px-4 py-2 rounded-md shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#eec064]"
+                            >
+                                <FaMapMarkerAlt className="text-2xl md:text-5xl" />
+                                <span className="font-medium ml-2">Gachibowli</span>
+                            </a>
                         </div>
-                        <div className="flex space-x-4">
-                            <div
-                                className="overflow-hidden rounded-lg"
-                                dangerouslySetInnerHTML={{
-                                    __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.75140611615!2d78.45640297468381!3d17.42371380175852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb974c629a05bf%3A0x1f2ad73d4257ca81!2sHyderabad%20Forex%20Limited!5e0!3m2!1sen!2sin!4v1715873066743!5m2!1sen!2sin&z=15" width="180" height="180" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-                                }}
-                            />
-                            <div
-                                className="overflow-hidden rounded-lg"
-                                dangerouslySetInnerHTML={{
-                                    __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.4985901306195!2d78.36422557468401!3d17.43583440140857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93ec2b8933c7%3A0xfe8d87caf8af1578!2sHYDERABAD%20FOREX%20LIMITED!5e0!3m2!1sen!2sin!4v1715872909969!5m2!1sen!2sin&z=15" width="180" height="180" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-                                }}
-                            />
-                        </div>
+
                     </div>
                 </div>
             </div>
