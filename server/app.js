785b02ec75faab70c708ec176948259a2155b995
Teja Bhargav Podila <tejabhargavpodila@gmail.com>
2024-08-05 14:04:12 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 // Connect to database
 connectDB();
 
-// app.use(cors()); 
+app.use(cors({
+    referer: process.env.BaseURL + '/' // Replace 'http://example.com' with the address you want to whitelist
+}));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
